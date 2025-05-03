e51fb3b5470f5546f686d4adea4da366c66ec744
Yves Schumann <y.schumann@proton.me>
2025-05-03 17:30:12 +0200

    Initial commit based on HACS blueprint repository

---
+++ custom_components/shadow-control/const.py
@@ -0,0 +1,8 @@
+"""Constants for shadow-control."""
+
+from logging import Logger, getLogger
+
+LOGGER: Logger = getLogger(__package__)
+
+DOMAIN = "shadow-control"
+ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
