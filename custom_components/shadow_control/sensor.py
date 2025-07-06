5b0019e092d1a1f2aa733ff4f257f938e2901c77
Yves Schumann <y.schumann@proton.me>
2025-07-06 17:59:04 +0200

    Use the same icon for sensor_current_state_text as for sensor_current_state

--- custom_components/shadow_control/sensor.py
+++ custom_components/shadow_control/sensor.py
@@ -158,6 +158,7 @@ class ShadowControlCurrentStateTextSensor(SensorEntity):
         self._attr_device_class = SensorDeviceClass.ENUM
         self._attr_translation_key = "sensor_current_state_text"  # translation key
         self._attr_has_entity_name = True
+        self._attr_icon = "mdi:state-machine"
 
         # Possible options as a list of strings (lowercase names of enum members)
         self._attr_options = [state.to_ha_state_string() for state in ShutterState]
